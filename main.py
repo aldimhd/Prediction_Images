cea0b89cf84f0b2c70d557144944679f3c7cc7c9
Moch. Chamdani Mustaqim <chamdani@outlook.com>
2021-10-28 04:41:05 +0700

    deploy model

---
+++ main.py
@@ -0,0 +1,74 @@
+import time
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from flask import Flask, request, render_template
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+RESULT_FOLDER = 'static/result'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'} 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+datahasil = os.listdir('static/result/')
+
+print("Init Flask App")
+class_list = {'GLIOMA': 0, 'MENINGIOMA': 1, 'PITUITARY': 2}
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+# Error Handle
+@app.route("/")
+def index():
+    return render_template('/select.html', )
+
+@app.route('/predict', methods=['POST'])
+def predicts_select():
+
+    chosen_model = request.form['select_model']
+    model_dict = {'h5 model'   :   'static/model/save_model.h5'  ,
+                  'json model' :   ['static/model/save_model.json','static/model/save_weights.h5'],
+                  }
+
+    if chosen_model in model_dict:
+        if "tl" in chosen_model:
+            json_file = open(model_dict[chosen_model][0], 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            model = model_from_json(loaded_model_json)
+            model.load_weights(model_dict[chosen_model][1])
+        else:
+            model = load_model(model_dict[chosen_model]) 
+    else:
+        model = load_model(model_dict[0])
+
+    file = request.files["file"]
+    file.save(os.path.join('static', 'temp.jpg'))
+    img = cv2.cvtColor(np.array(Image.open(file)), cv2.COLOR_BGR2RGB)
+    img = np.expand_dims(cv2.resize(img, model.layers[0].input_shape[0][1:3] if not model.layers[0].input_shape[1:3] else model.layers[0].input_shape[1:3]).astype('float32') / 255, axis=0)
+    start = time.time()
+    pred = model.predict(img)[0]
+    runtimes = round(time.time()-start,4)
+    respon_model = [round(elem * 100, 2) for elem in pred]
+
+    return predict_result_select(chosen_model, runtimes, respon_model, 'temp.jpg')
+
+def predict_result_select(model, run_time, probs, img):
+    idx_pred = probs.index(max(probs))
+    labels = list(class_list.keys())
+    return render_template('/result.html', labels=labels, 
+                            probs=probs, model=model, pred=idx_pred, 
+                            run_time=run_time, img=img)
+
+if __name__ == "__main__": 
+        app.run(debug=True, host='0.0.0.0', port=2000)
